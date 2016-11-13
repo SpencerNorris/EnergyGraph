283385ecc019ed78fde2a3ba43a22c8ce4ea5c73
Spencer Norris <spencercnorris2017@gmail.com>
2016-11-13 14:26:54 -0500

    more changes to munge.sh

--- munge.sh
+++ munge.sh
@@ -2,8 +2,9 @@
 SETLR_HOME=~/setlr
 EG_HOME=~/courses/datasci/EnergyGraph
 source ${SETLR_HOME}/venv/bin/activate
-
+cd data
 for setl in $(ls ${EG_HOME}/data/setlr/); do
+	echo "Munging" $setl
 	python ${SETLR_HOME}/setlr.py ${EG_HOME}/data/setlr/$setl
 done
 deactivate
