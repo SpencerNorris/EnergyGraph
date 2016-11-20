3317e2b56961ccf5fdbd731f50614b968d79c9f3
Spencer Norris <spencercnorris2017@gmail.com>
2016-11-20 11:38:56 -0500

    changed cd operation in munge.sh to use

--- munge.sh
+++ munge.sh
@@ -2,7 +2,7 @@
 SETLR_HOME=~/setlr
 EG_HOME=/home/spencer/courses/datasci/EnergyGraph
 source ${SETLR_HOME}/venv/bin/activate
-cd data
+cd ${EG_HOME}/data
 for setl in $(ls ${EG_HOME}/data/setlr/); do
 	echo "Munging" $setl
 	python ${SETLR_HOME}/setlr.py ${EG_HOME}/data/setlr/$setl
