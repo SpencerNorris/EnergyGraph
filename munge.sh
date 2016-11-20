665247b9c85ca2cc6585a4b636bbe0e690bd9784
Spencer Norris <spencercnorris2017@gmail.com>
2016-11-20 11:28:52 -0500

    small modification to munge.sh

--- munge.sh
+++ munge.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 SETLR_HOME=~/setlr
-EG_HOME=~/courses/datasci/EnergyGraph
+EG_HOME=/home/spencer/courses/datasci/EnergyGraph
 source ${SETLR_HOME}/venv/bin/activate
 cd data
 for setl in $(ls ${EG_HOME}/data/setlr/); do
