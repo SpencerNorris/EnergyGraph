5dfbcc3821492da8ab01ce99d7ce6a1006206993
Spencer Norris <spencercnorris2017@gmail.com>
2016-11-22 09:54:41 -0500

    added main.py; removed redundant namespaces from us-energy-model.owl

---
+++ src/main/python/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from rdflib import *
+import csv
+import json
+import requests
+import sys, os
+
+
+
+def main():
+	return 0
+
+if __name__ == '__main__':
+	sys.exit(main())
