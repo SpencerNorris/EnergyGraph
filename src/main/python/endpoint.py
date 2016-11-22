5c0f3989b8dbcaac03a874cd44d8fe5d147a0edb
Spencer Norris <spencercnorris2017@gmail.com>
2016-11-22 10:40:58 -0500

    more Python magic

---
+++ src/main/python/endpoint.py
@@ -0,0 +1,13 @@
+
+
+
+class Stardog(Object):
+	def __init__(self):
+		continue		
+	def query_by_energy_source(self):
+		return 0
+
+	def query_by_energy_category(self):
+		return 0
+
+
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -7,7 +7,28 @@ import json
 import requests
 import sys, os
 
+#Local modules
+import regress
 
+#Globals
+
+
+#Helper methods
+def get_largest_production_by_source():
+	for file in files:
+		with open(file, 'r') as csv_file:
+			reader = csv.DictReader(csv_file)
+
+
+#Main processing methods
+def regression():
+	return 0
+
+def query():
+	return 0
+
+def process():
+	return 0
 
 def main():
 	return 0
