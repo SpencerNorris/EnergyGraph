f87ebb5f246530fff726407f1477918eeef002bb
Spencer Norris <spencercnorris2017@gmail.com>
2016-11-25 16:07:43 -0500

    added shebangs to python scripts

--- src/main/python/endpoint.py
+++ src/main/python/endpoint.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
+from rdflib import SPARQLStore
 
 
-
-class Stardog(Object):
+class Stardog(SPARQLStore):
 	def __init__(self):
 		continue		
 	def query_by_energy_source(self):
@@ -10,4 +12,9 @@ class Stardog(Object):
 	def query_by_energy_category(self):
 		return 0
 
+	'''
+	Returns the energy sources responsible for the largest generation amount 
+	per state per year
+	'''
+	def get_largest_production_by_source():    
 
